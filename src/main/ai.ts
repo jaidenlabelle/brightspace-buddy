e7e46cf7402617e525b76efd8370998ebfbc2c91
Jaiden <jaidendestroyer@gmail.com>
2026-04-01 11:24:35 -0400

    Working AI proof-of-concept

--- src/main/ai.ts
+++ src/main/ai.ts
@@ -1,10 +1,29 @@
-import { LMStudioClient } from "@lmstudio/sdk";
-const client = new LMStudioClient();
+import { Chat, LMStudioClient } from "@lmstudio/sdk";
+
+const client = new LMStudioClient(  );
 
 export async function testAI() {
   console.log("Testing AI integration...");
-  const model = await client.llm.model("qwen3.5-9b-uncensored-hauhaucs-aggressive");
-  const result = await model.respond("What is the meaning of life?");
+  const client = new LMStudioClient({
+    baseUrl: "ws://24.246.0.21:1234",
+  });
 
-  console.info("AI Response:", result.content);
+  const model = await client.llm.model("qwen3.5-9b-uncensored-hauhaucs-aggressive");
+  const fileHandle = await client.files.prepareFile("Midterm Review.pdf");
+  const parsedDocument = await client.files.parseDocument(fileHandle, {
+    onParserLoaded: (parser) => {
+      console.log(`Using parser: ${parser.library} v${parser.version}`);
+    },
+    onProgress: (progress) => {
+      console.log(`Parsing: ${Math.round(progress * 100)}%`);
+    }
+  });
+  console.log("Parsed Document Content:", parsedDocument.content);
+
+  const result = await model.respond([
+    { role: "user", content: "Summarize this document in a paragraph under 150 words please" },
+    { role: "user", content: parsedDocument.content }
+  ]);
+
+  console.log("AI Response:", result.nonReasoningContent);
 }
